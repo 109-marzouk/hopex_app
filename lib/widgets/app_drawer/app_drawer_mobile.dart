134dad0d709ca2eb67298f8a9f2b929f92700832
Mohamed Marzouk <32062205+MohamedMarzouk23@users.noreply.github.com>
2020-04-10 15:31:04 +0200

    add some features

---
+++ lib/widgets/app_drawer/app_drawer_mobile.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hopex_progx/app_localizations.dart';
+
+import 'app_drawer.dart';
+
+class MyBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
+  }
+}
+
+class AppDrawerMobile extends StatelessWidget {
+  const AppDrawerMobile({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextDirection currentDirection = Directionality.of(context);
+    final bool isRTL = (currentDirection == TextDirection.rtl);
+    //final EdgeInsetsGeometry sts_point = isRTL ? EdgeInsets.only(value) : ;
+    var orientation = MediaQuery.of(context).orientation;
+    return Container(
+      //width: orientation == Orientation.portrait ? 300 : 55,
+      width: 300,
+      decoration: BoxDecoration(color: Colors.grey.shade100, boxShadow: [
+        BoxShadow(
+          blurRadius: 16,
+          color: Colors.black12,
+        )
+      ]),
+      child: ScrollConfiguration(
+        behavior: MyBehavior(),
+        child: ListView(
+          physics: orientation == Orientation.landscape ? ClampingScrollPhysics() : NeverScrollableScrollPhysics(),
+          padding: EdgeInsets.all(0),
+          children: <Widget>[
+            Container(
+              color: Colors.grey.shade100,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 40.0, bottom: 10, left: 20, right: 20),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Stack(
+                          children: <Widget>[
+                            new Container(
+                              width: 65.0,
+                              height: 65.0,
+                              decoration: new BoxDecoration(
+                                color: const Color(0xff7c94b6),
+                                image: new DecorationImage(
+                                  image: new ExactAssetImage('assets/images/progx.jpg'),
+                                  fit: BoxFit.cover,
+                                ),
+                                borderRadius: new BorderRadius.all(new Radius.circular(50.0)),
+                                border: new Border.all(
+                                  color: Colors.green.shade100,
+                                  width: 4.0,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(top: 49, left: !isRTL ? 45 : 0, right: isRTL ? 40 : 0),
+                              height: 15,
+                              width: 15,
+                              decoration: BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.all(
+                                      new Radius.circular(50.0)
+                                  )
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 10,),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text("Mohamed Marzouk", style: TextStyle(fontSize: 20, height: 1.7),),
+                              Text("1515\$", style: TextStyle(fontSize: 15, height: 1),),
+                              Text("250-726-943", style: TextStyle(fontSize: 15, color: Colors.black54, height: 1.2),),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 15,),
+                    Container(
+                      width: double.infinity,
+                      child: RaisedButton(
+                        elevation: 0,
+                        color: Colors.green,
+                        onPressed: (){},
+                        child: Text(AppLocalizations.of(context).translate("deposit"), style: TextStyle(color: Colors.white, fontSize: 15),),
+                      )
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20, child: Container(color: Colors.white,),),
+            Container(
+              height: MediaQuery.of(context).size.height,
+              color: Colors.white,
+              child: Column(children: AppDrawer.getDrawerOptions(context),
+              mainAxisAlignment: MainAxisAlignment.start,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/drawer_option/drawer_option_tablet.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:hopex_progx/hand_cursor.dart';
+
+class DrawerOptionTabletLandscape extends StatelessWidget {
+  final String title;
+  final IconData iconData;
+  final String toPage;
+  const DrawerOptionTabletLandscape({
+    Key key,
+    this.title,
+    this.iconData, this.toPage,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final String currRoute = ModalRoute.of(context).settings.name;
+    return HandCursor(
+      child: MaterialButton(
+        highlightElevation: 0,
+        hoverElevation: 0,
+        minWidth: double.infinity,
+        shape: (currRoute == toPage || currRoute == toPage) ? Border(
+            left: BorderSide(color: Colors.green, width: 3)
+        ) : null,
+        onPressed: () => Navigator.of(context).pushReplacementNamed(toPage),
+        splashColor: Colors.green.shade200,
+        highlightColor: Colors.green.shade200,
+        elevation: 0,
+        color: (currRoute == toPage || currRoute == toPage) ? Colors.grey.shade100 : Colors.transparent,
+        child: Container(
+          padding: EdgeInsets.only(top: 15, bottom: 15, right: 5, left: 5),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Icon(
+                iconData,
+                size: 25,
+                color: (currRoute == toPage || currRoute == toPage) ?
+                Colors.green : Colors.black,
+              ),
+              SizedBox(height: 5,),
+              Text(title, style: TextStyle(fontSize: 13, color: Colors.black54)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
